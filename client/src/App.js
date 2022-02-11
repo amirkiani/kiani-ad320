77cd4c49407bbe381358daca90090c9536c9d548
Amir Kiani <53153060+amirkiani@users.noreply.github.com>
2022-02-10 23:13:37 -0800

    started Notecard App

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import Topbar from './components/Topbar/Topbar'
+import CardNavigation from './components/CardNavigation/CardNavigation';
+import FlashCard from './components/FlashCard/FlashCard';
+
+function App() {
+  return (
+    <React.Fragment>
+    <Topbar />
+    <div className = 'container'>
+    <CardNavigation />
+    <FlashCard  />
+
+
+    </div>
+    </React.Fragment>
+    
+  
+  );
+}
+
+export default App;
---
+++ client/src/components/Topbar/Topbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Topbar.css'
+
+function Topbar () {
+    return (<div className="bar">
+        <h2 class="bar-title">Notable</h2>
+        </div>)
+}
+
+export default Topbar
